1a342b498bf9f142eb9da0b422ae594a097d309d
yue <yueyingjun@163.com>
2018-01-06 16:25:39 +0800

    play

--- video/public/js/video.js
+++ video/public/js/video.js
@@ -2,10 +2,22 @@ window.onload=function () {
 
     var videoBox=document.querySelector(".video-box");
     var controls=document.querySelector(".controls");
+    var video=document.querySelector("video");
     videoBox.onmouseover=function () {
-        controls.style.display="block";
+        controls.style.display="flex";
     }
     videoBox.onmouseout=function () {
         controls.style.display="none";
+    }
+    var playBtn=document.querySelector(".playBtn");
+    playBtn.onclick=function () {
+        console.log(video.paused)
+        if(video.paused){
+                video.play();
+            playBtn.style.backgroundImage="url(/img/pause.png)"
+        }else{
+            video.pause();
+            playBtn.style.backgroundImage="url(/img/play.png)"
+        }
     }
 }
